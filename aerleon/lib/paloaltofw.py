eeeeaf3c86301f6d2597a57e44d5e9c112bf2714
Jason Benterou <jason.benterou@gmail.com>
2022-10-04 14:56:58 -0700

    Faster address deduplication in PA addressbook builder

--- aerleon/lib/paloaltofw.py
+++ aerleon/lib/paloaltofw.py
@@ -452,6 +452,7 @@ class PaloAltoFW(aclgenerator.ACLGenerator):
                 )
 
             term_dup_check = set()
+            address_book_dup_check = set()
             new_terms = []
 
             for term in terms:
@@ -683,14 +684,19 @@ class PaloAltoFW(aclgenerator.ACLGenerator):
                     term.destination_address = daddr_split
 
                 # Build address book for the addresses referenced in the term.
-                for addr in term.source_address:
-                    if addr.version in exclude_address_family:
-                        continue
-                    self._BuildAddressBook(self.from_zone, addr)
-                for addr in term.destination_address:
-                    if addr.version in exclude_address_family:
-                        continue
-                    self._BuildAddressBook(self.to_zone, addr)
+                zones = collections.OrderedDict()
+                zones[self.from_zone] = [
+                    addr
+                    for addr in term.source_address
+                    if addr.version not in exclude_address_family
+                ]
+                zones[self.to_zone] = [
+                    addr
+                    for addr in term.destination_address
+                    if addr.version not in exclude_address_family
+                ]
+
+                self._BuildAddressBook(zones, address_book_dup_check)
 
                 # Handle ICMP/ICMPv6 terms.
                 if term.icmp_type and (
@@ -825,24 +831,33 @@ class PaloAltoFW(aclgenerator.ACLGenerator):
 
             self.pafw_policies.append((header, ruleset, filter_options))
 
-    def _BuildAddressBook(self, zone, address):
+    def _BuildAddressBook(self, zones, memo):
         """Create the address book configuration entries.
 
         Args:
-          zone: the zone these objects will reside in
-          address: a naming library address object
+          zones: a mapping from zone name to a list of naming library address objects
+            that will reside in the zone
+          memo: a set used to check for duplicate addresses in the same zone/parent_token group
         """
-        if zone not in self.addressbook:
-            self.addressbook[zone] = collections.OrderedDict()
-        if address.parent_token not in self.addressbook[zone]:
-            self.addressbook[zone][address.parent_token] = []
-        name = address.parent_token
-        for ip in self.addressbook[zone][name]:
-            if str(address) == str(ip[0]):
-                return
-        counter = len(self.addressbook[zone][address.parent_token])
-        name = "%s_%s" % (name, str(counter))
-        self.addressbook[zone][address.parent_token].append((address, name))
+        for zone, address_list in zones.items():
+            if zone not in self.addressbook:
+                self.addressbook[zone] = collections.OrderedDict()
+
+            for address in address_list:
+                parent_token = address.parent_token
+                address_str = str(address)
+
+                if (zone, parent_token, address_str) in memo:
+                    continue
+
+                memo.add((zone, parent_token, address_str))
+
+                if parent_token not in self.addressbook[zone]:
+                    self.addressbook[zone][parent_token] = []
+
+                counter = len(self.addressbook[zone][parent_token])
+                name = "%s_%s" % (parent_token, str(counter))
+                self.addressbook[zone][parent_token].append((address, name))
 
     def _SortAddressBookNumCheck(self, item):
         """Used to give a natural order to the list of acl entries.
