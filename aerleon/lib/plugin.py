5bcca678f3af2a435f07dce8bea5856e68375622
Jason Benterou <jason.benterou@gmail.com>
2022-11-28 16:35:23 -0800

    Save fancy stuff for later

--- aerleon/lib/plugin.py
+++ aerleon/lib/plugin.py
@@ -50,7 +50,7 @@ https://python-poetry.org/docs/pyproject/#plugins
 ## BasePlugin
 
 All Aerleon plug-ins must extend aerleon.plugin.BasePlugin and implement the
-required methods __init__() and requestMetadata(). requestMetadata() must return
+required methods __init__() and RequestMetadata(). RequestMetadata() must return
 an instance of aerleon.plugin.PluginMetadata or raise
 aerleon.plugin.PluginCompatibilityError .
 
@@ -59,63 +59,14 @@ must implement the property .generators and return a dictionary
 mapping target names to class constructors.
 
 These generator classes must implement BaseGenerator and implement the required
-methods __init__(), .configuration and generate(). Property .configuration must
-return an instance of aerleon.plugin.GeneratorConfiguration .
+methods __init__() and Generate().
 
-Generators may extend the policy file format by recognizing new keywords or
-keyword values in the Terms, Header or Target sections. The optional methods
-RecognizeKeyword(), RecognizeKeywordValue() and RecognizeOption() can be
-implemented for this purpose. See 'Recognizing New Keywords and Values' below.
-
-
-# Recognizing New Keywords and Values
-
-A generator plug-in can extend the list of recognized keywords in a policy file
-or extend the possible valid values on the right side of a key-value expression.
-
-RecognizeKeyword(self, context) -> RecognizerKeywordResult
-
-When Aerleon encounters an unrecognized keyword in a policy file it will invoke
-RecognizeKeyword() against all active generator plug-ins (called "surveying").
-Both target generators for the current filter and other generators are consulted.
-Aerleon will ignore the keyword with a "Keyword Unrecognized" warning if no
-active generators recognize it. Aerleon will ignore the keyword with a "Keyword
-Unused" warning if one or more active generators recognizes it but none of the
-target generators for the current filter do. Aerleon will halt processing the
-filter with a "Security-Critical Keyword Unused" error if any active generator
-has flagged this keyword as Security Critical and one or more target generator
-does not recognize it.
-
-RecognizeKeywordValue(self, context) -> RecognizerValueResult
-
-Every value on the right side of a key-value expression is checked against
-the RecognizeKeywordValue method of all target generators for the current filter.
-Generators can mostly ignore this method or return None for most inputs. Any
-value returned by RecognizeKeywordValue() will be installed in the 'extras'
-section of the Terms, Header or Target data model.
-
-RecognizeOption(self, context) -> RecognizerOptionResult
-
-Special processing is performed for the 'options' keyword in a Term section.
-Similar to RecognizeKeyword, Aerleon will survey all active generators in order
-to determine if an option is considered Security Critical. Aerleon will halt
-processing the filter with the error "Security-Critical Option Ignored" if one
-or more target generators would ignore a Security Critical option.
 """
 
 from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum
-import typing
-
-if typing.TYPE_CHECKING:
-    from aerleon.lib.recognizers import (
-        RecognizerContext,
-        RecognizerKeywordResult,
-        RecognizerOptionResult,
-        RecognizerValueResult,
-    )
 
 
 class PluginCompatibilityError(BaseException):
@@ -160,29 +111,8 @@ class BasePlugin:
         raise NotImplementedError
 
 
-@dataclass
-class GeneratorConfiguration:
-    target: str
-    useAddressBook: bool
-    useFlatten: bool
-    useMutable: bool = False
-
-
 class BaseGenerator:
     def __init__(self):
-        raise NotImplementedError
-
-    @property
-    def configuration(self) -> GeneratorConfiguration:
-        raise NotImplementedError
-
-    def RecognizeKeyword(self, _context: RecognizerContext) -> RecognizerKeywordResult:
-        raise NotImplementedError
-
-    def RecognizeKeywordValue(self, _context: RecognizerContext) -> RecognizerValueResult:
-        raise NotImplementedError
-
-    def RecognizeOption(self, _context: RecognizerContext) -> RecognizerOptionResult:
         raise NotImplementedError
 
     def generate(self):
