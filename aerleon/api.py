40eb9ec3cf3624651677a8b8ef9084fa6c6e537a
Jason Benterou <jason.benterou@gmail.com>
2023-01-14 00:00:19 -0800

    Switch API branch to use Naming.ParseDefinitionObject

--- aerleon/api.py
+++ aerleon/api.py
@@ -20,8 +20,7 @@ from aerleon.lib import policy_builder
 
 def Generate(
     policies,
-    service_names,
-    network_names,
+    definitions,
     output_directory: pathlib.Path = None,
     optimize=False,
     shade_check=False,
@@ -31,8 +30,7 @@ def Generate(
     context = multiprocessing.get_context()
     return _Generate(
         policies,
-        service_names,
-        network_names,
+        definitions,
         context,
         output_directory,
         optimize,
@@ -43,8 +41,7 @@ def Generate(
 
 def _Generate(
     policies,
-    service_names,
-    network_names,
+    definitions: naming.Naming,
     context: multiprocessing.context.BaseContext,
     output_directory=None,
     optimize=False,
@@ -52,10 +49,6 @@ def _Generate(
     exp_info=2,
     max_renderers=1,
 ) -> dict[str, str]:
-
-    definitions = naming.Naming()
-    definitions.ParseServiceList(_BuildNamingLines(service_names, "services"))
-    definitions.ParseNetworkList(_BuildNamingLines(network_names, "networks"))
 
     # thead-safe list for storing files to write
     manager: multiprocessing.managers.SyncManager = context.Manager()
@@ -199,53 +192,3 @@ def _GenerateACL(
             raise ACLGeneratorError(
                 'Error generating target ACL for %s:\n%s' % (filename, e)
             ) from e
-
-
-def _BuildNamingLines(data: dict[str, dict], definition_type: str):
-    """Convert to Naming line format.
-
-    Arguments:
-        data: A dictionary mapping names to item lists. Each item in the item
-            list should contain a name OR a protocol/port (if definition_type is
-            "services") OR an ip (if definition_type is "networks"). Each item
-            may contain a comment.
-        definition_type: A string containing either "services" or "networks". This
-            should correspond to the type of data present in the 'data' argument.
-
-    Returns:
-        A list of strings which can be understood by the Naming constructor.
-    """
-    # Note: eventually this should get extracted to a NamingBuilder pattern
-    # and share with YAML address books
-
-    lines = []
-    for name, item_list in data.items():
-        if not item_list:
-            continue
-
-        found_items = []
-        for item in item_list:
-            content = None
-            comment = item.get("comment")
-            if "name" in item:
-                content = item['name']
-            elif definition_type == "services":
-                content = f"{item['port']}/{item['protocol']}"
-            elif definition_type == "networks":
-                content = f"{item['ip']}"
-            else:
-                raise TypeError("Unexpected definition type")
-
-            if comment is not None:
-                found_items.append(f"{content} # {comment}")
-            else:
-                found_items.append(content)
-
-        new_lines = []
-        new_lines.append(f"{name} = {found_items[0]}")
-
-        for item in found_items[1:]:
-            new_lines.append(f"    {found_items[0]}")
-
-        lines.extend(new_lines)
-    return lines
--- tests/api/api_test.py
+++ tests/api/api_test.py
@@ -2,6 +2,8 @@ import re
 from absl.testing import absltest
 
 from aerleon import api
+from aerleon.lib import naming
+
 
 GOOD_POLICY_1 = {
     "filename": "raw_policy_all_builtin",
@@ -25,134 +27,159 @@ GOOD_POLICY_1 = {
     ],
 }
 
-GOOD_PORTS_1 = {
-    "SVC1": [
-        {
-            "protocol": "tcp",
-            "port": 80,
-        },
-        {
-            "protocol": "udp",
-            "port": 81,
-        },
-        {
-            "protocol": "tcp",
-            "port": 82,
-        },
-    ],
-    "SVC2": [
-        {
-            "protocol": "tcp",
-            "port": 80,
-        },
-        {
-            "protocol": "udp",
-            "port": 81,
-        },
-        {
-            "protocol": "tcp",
-            "port": 82,
-        },
-        {
-            "name": "SVC2",
-        },
-    ],
-    "SVC3": [
-        {
-            "protocol": "tcp",
-            "port": 80,
-        },
-        {
-            "protocol": "udp",
-            "port": 81,
-        },
-    ],
-    "SVC4": [
-        {"protocol": "tcp", "port": 80, "comment": "some service"},
-    ],
-    "TCP_90": [
-        {
-            "protocol": "tcp",
-            "port": 90,
-        },
-    ],
-    "SVC5": [
-        {
-            "name": "TCP_90",
-        },
-    ],
-    "SVC6": [
-        {
-            "name": "SVC1",
-        },
-        {
-            "name": "SVC5",
-        },
-    ],
+SERVICES_1 = {
+    "services": {
+        "SVC1": [
+            {
+                "protocol": "tcp",
+                "port": 80,
+            },
+            {
+                "protocol": "udp",
+                "port": 81,
+            },
+            {
+                "protocol": "tcp",
+                "port": 82,
+            },
+        ],
+        "SVC2": [
+            {
+                "protocol": "tcp",
+                "port": 80,
+            },
+            {
+                "protocol": "udp",
+                "port": 81,
+            },
+            {
+                "protocol": "tcp",
+                "port": 82,
+            },
+            {
+                "name": "SVC2",
+            },
+        ],
+        "SVC3": [
+            {
+                "protocol": "tcp",
+                "port": 80,
+            },
+            {
+                "protocol": "udp",
+                "port": 81,
+            },
+        ],
+        "SVC4": [
+            {"protocol": "tcp", "port": 80, "comment": "some service"},
+        ],
+        "TCP_90": [
+            {
+                "protocol": "tcp",
+                "port": 90,
+            },
+        ],
+        "SVC5": [
+            {
+                "name": "TCP_90",
+            },
+        ],
+        "SVC6": [
+            {
+                "name": "SVC1",
+            },
+            {
+                "name": "SVC5",
+            },
+        ],
+    }
 }
 
 
-GOOD_IPS_1 = {
-    "NET1": [
-        {
-            "ip": "10.1.0.0/8",
-            "comment": "network1",
+NETWORKS_1 = {
+    "networks": {
+        "NET1": {
+            "values": [
+                {
+                    "ip": "10.1.0.0/8",
+                    "comment": "network1",
+                },
+            ]
         },
-    ],
-    "NET2": [
-        {
-            "ip": "10.2.0.0/16",
-            "comment": "network2.0",
+        "NET2": {
+            "values": [
+                {
+                    "ip": "10.2.0.0/16",
+                    "comment": "network2.0",
+                },
+                {
+                    "name": "NET1",
+                    "comment": "network2.0",
+                },
+            ]
         },
-        {
-            "name": "NET1",
-            "comment": "network2.0",
+        "9OCLOCK": {
+            "values": [
+                {
+                    "ip": "1.2.3.4/32",
+                    "comment": "9 is the time",
+                },
+            ]
         },
-    ],
-    "9OCLOCK": [
-        {
-            "ip": "1.2.3.4/32",
-            "comment": "9 is the time",
+        "FOOBAR": {
+            "values": [
+                {
+                    "name": "9OCLOCK",
+                },
+            ]
         },
-    ],
-    "FOOBAR": [
-        {
-            "name": "9OCLOCK",
+        "FOO_V6": {
+            "values": [
+                {
+                    "ip": "::FFFF:FFFF:FFFF:FFFF",
+                },
+            ]
         },
-    ],
-    "FOO_V6": [
-        {
-            "ip": "::FFFF:FFFF:FFFF:FFFF",
+        "BAR_V6": {
+            "values": [
+                {
+                    "ip": "::1/128",
+                },
+            ]
         },
-    ],
-    "BAR_V6": [
-        {
-            "ip": "::1/128",
+        "BAZ": {
+            "values": [
+                {
+                    "name": "FOO_V6",
+                },
+                {
+                    "name": "BAR_V6",
+                },
+            ]
         },
-    ],
-    "BAZ": [
-        {
-            "name": "FOO_V6",
+        "BING": {
+            "values": [
+                {
+                    "name": "NET1",
+                    "comment": "foo",
+                },
+                {
+                    "name": "BAR_V6",
+                },
+            ]
         },
-        {
-            "name": "BAR_V6",
-        },
-    ],
-    "BING": [
-        {
-            "name": "NET1",
-            "comment": "foo",
-        },
-        {
-            "name": "BAR_V6",
-        },
-    ],
+    }
 }
 
 
 class ApiTest(absltest.TestCase):
     def testGenerate(self):
-        configs = api.Generate([GOOD_POLICY_1], GOOD_PORTS_1, GOOD_IPS_1)
+
+        definitions = naming.Naming()
+        definitions.ParseDefinitionsObject(SERVICES_1, "blah")
+        definitions.ParseDefinitionsObject(NETWORKS_1, "blah")
+
+        configs = api.Generate([GOOD_POLICY_1], definitions)
         acl = configs["raw_policy_all_builtin.acl"]
         self.assertTrue(re.search(' deny-to-reserved', str(acl)))
         print(acl)
