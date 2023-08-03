a6ff32a08ea0b21e722e4c456e51ea8bb3af052e
Rob Ankeny <ankenyr@gmail.com>
2023-08-03 06:40:57 +0000

    Removing conflicts.

--- aerleon/lib/paloaltofw.py
+++ aerleon/lib/paloaltofw.py
@@ -995,10 +995,6 @@ class PaloAltoFW(aclgenerator.ACLGenerator):
                 else:
                     for x in options["source"]:
                         if no_addr_obj:
-<<<<<<< HEAD
-=======
-                            pass
->>>>>>> main
                             for ip in self.addressbook.GetAddress('', x):
                                 member = etree.SubElement(source, "member")
                                 member.text = str(ip)
@@ -1033,10 +1029,6 @@ class PaloAltoFW(aclgenerator.ACLGenerator):
                 else:
                     for x in options["destination"]:
                         if no_addr_obj:
-<<<<<<< HEAD
-=======
-                            pass
->>>>>>> main
                             for ip in self.addressbook.GetAddress('', x):
                                 member = etree.SubElement(dest, "member")
                                 member.text = str(ip)
