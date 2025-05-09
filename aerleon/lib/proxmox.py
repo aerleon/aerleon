16c410e80381e6e27d10e2080726ac8245218024
lou lecrivain <lou.lecrivain@wdz.de>
2025-05-09 17:26:00 +0200

    2nd version of draft for proxmox firewall

--- aerleon/lib/proxmox.py
+++ aerleon/lib/proxmox.py
@@ -5,13 +5,45 @@ from aerleon.lib.nacaddr import IPv6, IPv4
 from aerleon.lib.policy import Policy
 
 
+class Error(aclgenerator.Error):
+    pass
+
+
+class UnsupportedFilterOptionError(Error):
+    pass
+
+
+class ProxmoxPort:
+    @staticmethod
+    def _singlePortFmt(port: int) -> str:
+        return str(port)
+
+    @staticmethod
+    def _portTupleFmt(port: tuple) -> str:
+        if port[0] == port[1]:
+            return str(port[0])
+        else:
+            return '%d-%d' % port
+
+    def __init__(self, port: Union[Tuple[int, int], int]):
+        self.str_representation = ''
+        if isinstance(port, int):
+            self.str_representation = self._singlePortFmt(port)
+        elif isinstance(port, tuple):
+            self.str_representation = self._portTupleFmt(port)
+
+    def __str__(self):
+        return self.str_representation
+
+
 class Term(aclgenerator.Term):
     """
     (internal) representation of an individual proxmox-firewall term
     takes generic Term for conversion to internal type as an argument
-    direction is IN, OUT, FORWARD
+    direction is IN, OUT, FORWARD (regardless of zone type, zone
+    constraints should be handled by the client class)
     """
-    _GOOD_DIRECTIONS = ['IN', 'OUT', 'FORWARD']
+    GOOD_DIRECTIONS = [ 'IN', 'OUT', 'FORWARD' ]
     _ACTIONS = {
         'accept': 'ACCEPT',
         'deny': 'DENY',
@@ -24,27 +56,28 @@ class Term(aclgenerator.Term):
 
     def __str__(self) -> str:
         """ returns the proxmox-firewall string representation of the term """
-        ret = str()
-        options = [self.direction, self.term.action[0]]
+        ret_str = []
 
         def to_network_addr(i: Union[IPv6, IPv4]):
             return str(i.with_prefixlen)
 
-        if self.term.destination_address:
-            options.append("-dest " + ','.join(map(to_network_addr, self.term.destination_address)))
-        if self.term.source_address:
-            options.append("-source " + ','.join(map(to_network_addr, self.term.source_address)))
-        if self.term.source_port:
-            options.append("-sport " + ','.join(self.term.source_port))
-        if self.term.destination_port:
-            options.append("-dport " + ','.join(self.term.source_port))
-        if self.term.protocol:
-            options.append("-proto " + self.term.protocol[0])
+        # proxmox firewall only supports one protocol per rule
+        for protocol in self.term.protocol:
+            options = [ self.direction, self.term.action[0], "-proto %s" % protocol ]
+            # proxmox firewall supports multiple sources/destinations per rule
+            if self.term.destination_address:
+                options.append("-dest %s" % ','.join(map(to_network_addr, self.term.destination_address)))
+            if self.term.source_address:
+                options.append("-source %s" % ','.join(map(to_network_addr, self.term.source_address)))
+            if self.term.source_port:
+                options.append("-sport %s" % ','.join(map(lambda p: str(ProxmoxPort(p)), self.term.source_port)))
+            if self.term.destination_port:
+                options.append("-dport %s" % ','.join(map(lambda p: str(ProxmoxPort(p)), self.term.destination_port)))
+            if self.term.comment:
+                options.append("# %s" % ' '.join(self.term.comment))
+            ret_str.append(' '.join(options))
 
-        # should be in [IPSET something]
-        # self.term.destination_prefix
-        # self.term.source_prefix
-        return ret + ' '.join(options)
+        return '\n'.join(ret_str)
 
 
 class Proxmox(aclgenerator.ACLGenerator):
@@ -53,14 +86,20 @@ class Proxmox(aclgenerator.ACLGenerator):
     # aerleon class props
     _PLATFORM = 'proxmox'
     SUFFIX = '.fw'
+    _TERM = Term
     # own class props
     _LOG_LEVELS = {
     }
-    _OPTIONS = {
+    _ZONES = [ "host", "vm", "vnet" ]
+    _BY_ZONE = {
+        "host": {
+            "supported_directions": [ "IN", "OUT", "FORWARD" ]
+        },
+        "vm": {
+            "supported_directions": [ "IN", "OUT" ],
+        },
         "vnet": {
-            'enable': ['0', '1'],
-            'log_level_forward': ['alert', 'crit', 'debug', 'emerg', 'err', 'info', 'nolog', 'notice', 'warning'],
-            'policy_forward': ['ACCEPT', 'DROP'],
+            "supported_directions": [ "FORWARD" ],
         }
     }
 
@@ -71,6 +110,8 @@ class Proxmox(aclgenerator.ACLGenerator):
     def _BuildTokens(self) -> Tuple[Set[str], Dict[str, Set[str]]]:
         """ returns the list of DSL + YAML supplementary tokens supported (proxmox-firewall specific) """
         supported_tokens, supported_sub_tokens = super()._BuildTokens()
+        # TODO fixme, can use in_interface or out_interface for this
+        # TODO native logging can also be used
         supported_tokens |= {
             'network_interface',
             'logging',
@@ -83,24 +124,43 @@ class Proxmox(aclgenerator.ACLGenerator):
     def _TranslatePolicy(self, pol: policy.Policy, exp_info: int) -> None:
         for header, terms in pol.filters:
             self.filter_options = header.FilterOptions(self._PLATFORM)
-            filter_name = self.filter_options[0]
+            valid_options = self._ZONES + self._TERM.GOOD_DIRECTIONS
+
+            for opt in self.filter_options:
+                if opt not in valid_options:
+                    raise UnsupportedFilterOptionError("unknown options")
+
+            if len(self.filter_options) < 2:
+                raise UnsupportedFilterOptionError("missing options, zone and direction are mandatory")
+
+            filter_zone = self.filter_options[0]
+            filter_direction = self.filter_options[1]
+
+            if filter_zone not in self._ZONES:
+                raise UnsupportedFilterOptionError("unknown zone")
+
+            if filter_direction not in self._BY_ZONE[filter_zone]["supported_directions"]:
+                raise UnsupportedFilterOptionError(
+                    "direction " + filter_direction + " not supported for zone type " + filter_zone,
+                )
+
             new_terms = []
             for term in terms:
                 new_terms.append(
                     Term(
                         term,
-                        'FORWARD'
+                        filter_direction,
                     )
                 )
             self.proxmox_policies.append(
-                (header, filter_name, new_terms)
+                (header, filter_zone, filter_direction, new_terms)
             )
 
     def __str__(self):
         target = []
-        for header, filter_name, terms in self.proxmox_policies:
+        for header, zone, direction, terms in self.proxmox_policies:
             for term in terms:
                 term_str = str(term)
                 if term_str:
                     target.append(term_str)
-        return '\n'.join(target)
+        return '\n'.join(target) + '\n'
