dcc8634f58796f085d8b18ac73eeb60bd297a4f6
lou lecrivain <lou.lecrivain@wdz.de>
2025-05-13 19:32:05 +0200

    add support for proxmox zone options

--- aerleon/lib/proxmox.py
+++ aerleon/lib/proxmox.py
@@ -1,16 +1,122 @@
-from typing import Tuple, Set, Dict, Union
+import math
+from abc import abstractmethod, ABCMeta
+from typing import Tuple, Set, Dict, Union, List
 
 from aerleon.lib import aclgenerator, policy
 from aerleon.lib.nacaddr import IPv6, IPv4
 from aerleon.lib.policy import Policy
 
 
+### error classes ###
 class Error(aclgenerator.Error):
     pass
 
 
 class UnsupportedFilterOptionError(Error):
     pass
+
+### helper classes ###
+class AbstractOption(metaclass=ABCMeta):
+    def __init__(self, config: dict, *args, **kwargs):
+        self.config_ref = config
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.getKey())
+
+    @abstractmethod
+    def ingest(self, token: str) -> bool:
+        # should return true if token is ingested
+        pass
+
+    @abstractmethod
+    def complete(self) -> bool:
+        pass
+
+    @abstractmethod
+    def getKey(self) -> str:
+        pass
+
+
+class BooleanKeywordOption(AbstractOption):
+    def __init__(self, key: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.key = key
+
+    def getKey(self) -> str:
+        return self.key
+
+    def ingest(self, token: str) -> bool:
+        got_token = False
+        if token == self.key:
+            self.config_ref[self.key] = "1" # perl
+            got_token = True
+        return got_token
+
+    def complete(self) -> bool:
+        return True
+
+
+class AbstractValueOption(AbstractOption, metaclass=ABCMeta):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.key_ingested = False
+        self.any_value_ingested = False
+
+    @abstractmethod
+    def tokenValidationTemplateMethod(self, token: str) -> bool:
+        pass
+
+    def configInsertTemplateMethod(self, token: str):
+        self.config_ref[self.getKey()] = token
+
+    def ingest(self, token: str) -> bool:
+        got_token = False
+        if token == self.getKey():
+            self.key_ingested, got_token = True, True
+        elif self.tokenValidationTemplateMethod(token) and self.key_ingested:
+            self.any_value_ingested, got_token = True, True
+            self.configInsertTemplateMethod(token)
+        return got_token
+
+    def complete(self) -> bool:
+        return (
+                (self.key_ingested, self.any_value_ingested) == (False, False)
+                or self.key_ingested and self.any_value_ingested
+        )
+
+
+class ValueOption(AbstractValueOption):
+    def __init__(self, *args, **kwargs: List[str]):
+        super().__init__(*args, **kwargs)
+        self.key = list(kwargs.keys())[0]
+        self.values = kwargs[self.key]
+
+    def getKey(self) -> str:
+        return self.key
+
+    def tokenValidationTemplateMethod(self, token: str) -> bool:
+        return token in self.values
+
+
+class MultiValueOption(ValueOption):
+    def configInsertTemplateMethod(self, token: str):
+        if self.getKey() not in self.config_ref.keys():
+            self.config_ref[self.getKey()] = []
+        self.config_ref[self.getKey()].append(token)
+
+
+class NumberValueOption(AbstractValueOption):
+    def __init__(self, key: str, lower: float, upper: float, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.key = key
+        self.lower = lower
+        self.upper = upper
+
+    def getKey(self) -> str:
+        return self.key
+
+    def tokenValidationTemplateMethod(self, token: str) -> bool:
+        return token.isdecimal() and self.lower <= float(token) <= self.upper
 
 
 class ProxmoxPort:
@@ -36,6 +142,7 @@ class ProxmoxPort:
         return self.str_representation
 
 
+### implementation ###
 class Term(aclgenerator.Term):
     """
     (internal) representation of an individual proxmox-firewall term
@@ -44,9 +151,9 @@ class Term(aclgenerator.Term):
     constraints should be handled by the client class)
     """
     GOOD_DIRECTIONS = [ 'IN', 'OUT', 'FORWARD' ]
-    _ACTIONS = {
+    ACTIONS_MAP = {
         'accept': 'ACCEPT',
-        'deny': 'DENY',
+        'deny': 'DROP',
         'reject': 'REJECT',
     }
 
@@ -63,7 +170,7 @@ class Term(aclgenerator.Term):
 
         # proxmox firewall only supports one protocol per rule
         for protocol in self.term.protocol:
-            options = [ self.direction, self._ACTIONS[self.term.action[0]], "-proto %s" % protocol ]
+            options = [self.direction, self.ACTIONS_MAP[self.term.action[0]], "-proto %s" % protocol]
             # proxmox firewall supports multiple sources/destinations per rule
             if self.term.source_interface:
                 options.append("-iface %s" % self.term.source_interface)
@@ -90,18 +197,67 @@ class Proxmox(aclgenerator.ACLGenerator):
     SUFFIX = '.fw'
     _TERM = Term
     # own class props
-    _LOG_LEVELS = {
-    }
-    _ZONES = [ "host", "vm", "vnet" ]
+    _LOG_LEVELS = [
+        "alert", "crit", "debug", "emerg", "error", "info", "nolog", "notice", "warning"
+    ]
+    _NF_CONNTRACK_HELPERS = [ "amanda", "ftp", "irc", "netbios-ns", "pptp", "sane", "sip", "snmp", "tftp" ]
     _BY_ZONE = {
+        "cluster": {
+            "supported_directions": _TERM.GOOD_DIRECTIONS,
+            "supported_options": lambda config : [
+                BooleanKeywordOption('enable', config),
+                BooleanKeywordOption('ebtables', config),
+                ValueOption(config, policy_forward=list(Proxmox._TERM.ACTIONS_MAP.values())),
+                ValueOption(config, policy_in=list(Proxmox._TERM.ACTIONS_MAP.values())),
+                ValueOption(config, policy_out=list(Proxmox._TERM.ACTIONS_MAP.values())),
+            ],
+        },
         "host": {
-            "supported_directions": [ "IN", "OUT", "FORWARD" ]
+            "supported_directions": _TERM.GOOD_DIRECTIONS,
+            "supported_options": lambda config : [
+                BooleanKeywordOption('enable', config),
+                ValueOption(config, log_level_forward=Proxmox._LOG_LEVELS),
+                ValueOption(config, log_level_in=Proxmox._LOG_LEVELS),
+                ValueOption(config, log_level_out=Proxmox._LOG_LEVELS),
+                BooleanKeywordOption('log_nf_conntrack', config),
+                BooleanKeywordOption('ndp', config),
+                BooleanKeywordOption('nf_conntrack_allow_invalid', config),
+                MultiValueOption(config, nf_conntrack_helpers=Proxmox._NF_CONNTRACK_HELPERS),
+                NumberValueOption('nf_conntrack_max', 32768, math.inf, config),
+                NumberValueOption('nf_conntrack_tcp_timeout_established', 7875, math.inf, config),
+                NumberValueOption('nf_conntrack_tcp_timeout_syn_recv', 30, 60, config),
+                BooleanKeywordOption('nftables', config),
+                BooleanKeywordOption('nosmurfs', config),
+                BooleanKeywordOption('protection_synflood', config),
+                NumberValueOption('protection_synflood_burst', 0, math.inf, config),
+                NumberValueOption('protection_synflood_rate', 0, math.inf, config),
+                ValueOption(config, smurf_log_level=Proxmox._LOG_LEVELS),
+                ValueOption(config, tcp_flags_log_level=Proxmox._LOG_LEVELS),
+                BooleanKeywordOption('tcp_flags', config),
+            ],
         },
         "vm": {
             "supported_directions": [ "IN", "OUT" ],
+            "supported_options": lambda config : [
+                BooleanKeywordOption('dhcp', config),
+                BooleanKeywordOption('enable', config),
+                BooleanKeywordOption('ipfilter', config),
+                ValueOption(config, log_level_in=Proxmox._LOG_LEVELS),
+                ValueOption(config, log_level_out=Proxmox._LOG_LEVELS),
+                BooleanKeywordOption('macfilter', config),
+                BooleanKeywordOption('ndp', config),
+                ValueOption(config, policy_in=list(Proxmox._TERM.ACTIONS_MAP.values())),
+                ValueOption(config, policy_out=list(Proxmox._TERM.ACTIONS_MAP.values())),
+                BooleanKeywordOption('radv', config),
+            ],
         },
         "vnet": {
             "supported_directions": [ "FORWARD" ],
+            "supported_options": lambda config : [
+                BooleanKeywordOption('enable', config),
+                ValueOption(config, log_level_forward=Proxmox._LOG_LEVELS),
+                ValueOption(config, policy_forward=list(Proxmox._TERM.ACTIONS_MAP.values())),
+            ],
         }
     }
 
@@ -124,11 +280,6 @@ class Proxmox(aclgenerator.ACLGenerator):
     def _TranslatePolicy(self, pol: policy.Policy, exp_info: int) -> None:
         for header, terms in pol.filters:
             self.filter_options = header.FilterOptions(self._PLATFORM)
-            valid_options = self._ZONES + self._TERM.GOOD_DIRECTIONS
-
-            for opt in self.filter_options:
-                if opt not in valid_options:
-                    raise UnsupportedFilterOptionError("unknown options")
 
             if len(self.filter_options) < 2:
                 raise UnsupportedFilterOptionError("missing options, zone and direction are mandatory")
@@ -136,12 +287,27 @@ class Proxmox(aclgenerator.ACLGenerator):
             filter_zone = self.filter_options[0]
             filter_direction = self.filter_options[1]
 
-            if filter_zone not in self._ZONES:
+            if filter_zone not in self._BY_ZONE.keys():
                 raise UnsupportedFilterOptionError("unknown zone")
 
             if filter_direction not in self._BY_ZONE[filter_zone]["supported_directions"]:
                 raise UnsupportedFilterOptionError(
                     "direction " + filter_direction + " not supported for zone type " + filter_zone,
+                )
+
+            filter_config = dict()
+            available_zone_options = self._BY_ZONE[filter_zone]['supported_options'](filter_config)
+            for t in self.filter_options[2:]:
+                ingested_options = []
+                for o in available_zone_options:
+                    ingested_options.append(o.ingest(t))
+                if not any(ingested_options):
+                    raise UnsupportedFilterOptionError("incorrect filter option directive %s" % t)
+            incomplete_options = list(filter(lambda o: not o.complete(), available_zone_options))
+            if incomplete_options:
+                raise UnsupportedFilterOptionError(
+                    "missing or incorrect value for filter option(s) %s",
+                    incomplete_options
                 )
 
             new_terms = []
@@ -153,12 +319,13 @@ class Proxmox(aclgenerator.ACLGenerator):
                     )
                 )
             self.proxmox_policies.append(
-                (header, filter_zone, filter_direction, new_terms)
+                (header, filter_zone, filter_direction, filter_config, new_terms)
             )
 
     def __str__(self):
         target = []
-        for header, zone, direction, terms in self.proxmox_policies:
+        for header, zone, direction, filter_config, terms in self.proxmox_policies:
+            target.append(str(filter_config))
             for term in terms:
                 term_str = str(term)
                 if term_str:
