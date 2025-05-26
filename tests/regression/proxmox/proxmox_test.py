fcba0e01dc109224e62cc2453454f4ae8b9b47ae
lou lecrivain <lou.lecrivain@wdz.de>
2025-05-26 16:39:33 +0200

    more tests, enhance coverage

--- tests/regression/proxmox/proxmox_test.py
+++ tests/regression/proxmox/proxmox_test.py
@@ -16,8 +16,43 @@ header {
 
 GOOD_HEADER_2 = """
 header {
-  comment:: "test acl for host zone"
+  comment:: "test acl for host zone IN"
   target:: proxmox host IN
+}
+"""
+
+GOOD_HEADER_3 = """
+header {
+  comment:: "test acl for host zone OUT"
+  target:: proxmox host OUT
+}
+"""
+
+GOOD_HEADER_4 = """
+header {
+  comment:: "test value option"
+  target:: proxmox host OUT log_level_in err
+}
+"""
+
+GOOD_HEADER_5 = """
+header {
+  comment:: "test boolean option"
+  target:: proxmox host IN ndp
+}
+"""
+
+GOOD_HEADER_6 = """
+header {
+  comment:: "test number value option"
+  target:: proxmox host IN nf_conntrack_max 32768
+}
+"""
+
+GOOD_HEADER_7 = """
+header {
+  comment:: "test multivalue option"
+  target:: proxmox host OUT nf_conntrack_helpers tftp irc sip
 }
 """
 
@@ -30,6 +65,18 @@ header {
 BAD_HEADER_2 = """
 header {
   target:: proxmox vm OUI
+}
+"""
+
+BAD_HEADER_3 = """
+header {
+  target:: proxmox vm
+}
+"""
+
+BAD_HEADER_4 = """
+header {
+  target:: proxmox
 }
 """
 
@@ -66,6 +113,32 @@ term good-term-4 {
   logging:: true
   option:: log_info
   action:: deny
+}
+"""
+
+GOOD_TERM_5 = """
+term good-term-5 {
+  comment:: "term with no source/destination addr"
+  protocol:: l2tp ipip gre esp
+  action:: deny
+}
+"""
+
+GOOD_TERM_6 = """
+term good-term-6 {
+  source-port:: GAMES
+  protocol:: tcp udp
+  action:: deny
+  logging:: true
+}
+"""
+
+GOOD_TERM_7 = """
+term good-term-7 {
+  destination-port:: WEB_ALT
+  protocol:: tcp
+  action:: deny
+  logging:: true
 }
 """
 
@@ -129,6 +202,10 @@ class ProxmoxFWTest(absltest.TestCase):
         self.naming._ParseLine('SOME_HOST6 = 2001:db8::2af/128', 'networks')
         self.naming._ParseLine('V4ANY = 0.0.0.0/0', 'networks')
         self.naming._ParseLine('NTP = 123/tcp 123/udp', 'services')
+        self.naming._ParseLine(
+            'GAMES = 1926/tcp 1926/udp 26000/tcp 26000/udp 666/tcp 666/udp', 'services'
+        )
+        self.naming._ParseLine('WEB_ALT = 8000-8800/tcp', 'services')
 
     def testBadZoneName(self):
         pol = policy.ParsePolicy(BAD_HEADER_1 + GOOD_TERM_1, self.naming)
@@ -146,6 +223,73 @@ class ProxmoxFWTest(absltest.TestCase):
         pol = policy.ParsePolicy(BAD_HEADER_2 + GOOD_TERM_1, self.naming)
         with self.assertRaises(UnsupportedFilterOptionError):
             proxmox.Proxmox(pol, EXP_INFO)
+
+    def testMissingArguments(self):
+        pol = policy.ParsePolicy(BAD_HEADER_3 + GOOD_TERM_1, self.naming)
+        with self.assertRaises(UnsupportedFilterOptionError):
+            proxmox.Proxmox(pol, EXP_INFO)
+        pol = policy.ParsePolicy(BAD_HEADER_4 + GOOD_TERM_1, self.naming)
+        with self.assertRaises(UnsupportedFilterOptionError):
+            proxmox.Proxmox(pol, EXP_INFO)
+
+    @capture.stdout
+    def testValueOption(self):
+        pol = policy.ParsePolicy(GOOD_HEADER_4 + GOOD_TERM_1, self.naming)
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        out = str(acl)
+        self.assertIn('log_level_in: err', out, "value option not present in final output")
+        print(out)
+
+    @capture.stdout
+    def testBooleanOption(self):
+        pol = policy.ParsePolicy(GOOD_HEADER_5 + GOOD_TERM_1, self.naming)
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        out = str(acl)
+        self.assertIn('ndp: 1', out, "boolean option not present in final output")
+        print(out)
+
+    @capture.stdout
+    def testNumberValueOption(self):
+        pol = policy.ParsePolicy(GOOD_HEADER_6 + GOOD_TERM_1, self.naming)
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        out = str(acl)
+        self.assertIn(
+            'nf_conntrack_max: 32768', out, "number value option not present in final output"
+        )
+        print(out)
+
+    @capture.stdout
+    def testMultiValueOption(self):
+        pol = policy.ParsePolicy(GOOD_HEADER_7 + GOOD_TERM_1, self.naming)
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        out = str(acl)
+        self.assertIn(
+            f"nf_conntrack_helpers: {','.join(sorted(['tftp','irc','sip']))}",
+            out,
+            "multivalue option not present in final output",
+        )
+        print(out)
+
+    @capture.stdout
+    def testOptionMerge(self):
+        pol = policy.ParsePolicy(
+            GOOD_HEADER_4
+            + GOOD_TERM_1
+            + GOOD_HEADER_5
+            + GOOD_TERM_1
+            + GOOD_HEADER_7
+            + GOOD_TERM_2,
+            self.naming,
+        )
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        out = str(acl)
+        default_msg = "missing option in final output, option merge failed?"
+        self.assertIn("log_level_in: err", out, default_msg)
+        self.assertIn("ndp: 1", out, default_msg)
+        self.assertIn(
+            f"nf_conntrack_helpers: {','.join(sorted(['tftp','irc','sip']))}", out, default_msg
+        )
+        print(out)
 
     @capture.stdout
     def testComment(self):
@@ -229,6 +373,39 @@ class ProxmoxFWTest(absltest.TestCase):
         output = str(acl)
         self.assertIn('-source 10.0.0.1/32', output)
         self.assertIn('-source 2001:db8::2af/128', output)
+        print(output)
+
+    @capture.stdout
+    def testPortAndPortRanges(self):
+        pol = policy.ParsePolicy(GOOD_HEADER_1 + GOOD_TERM_6, self.naming)
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        output = str(acl)
+        self.assertIn('-proto tcp -sport 666,1926,26000', output, 'ports not found')
+        self.assertIn('-proto udp -sport 666,1926,26000', output, 'ports not found')
+        print(output)
+        pol = policy.ParsePolicy(GOOD_HEADER_1 + GOOD_TERM_7, self.naming)
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        output = str(acl)
+        self.assertIn('-proto tcp -dport 8000-8800', output, 'port range not found')
+        print(output)
+
+    @capture.stdout
+    def testNoSourceNoDestinationTerm(self):
+        pol = policy.ParsePolicy(GOOD_HEADER_1 + GOOD_TERM_5, self.naming)
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        output = str(acl)
+        self.assertIn('IN DROP -proto', output)
+        print(output)
+
+    @capture.stdout
+    def testMixedDirectionPolicy(self):
+        pol = policy.ParsePolicy(
+            GOOD_HEADER_2 + GOOD_TERM_1 + GOOD_HEADER_3 + GOOD_TERM_1, self.naming
+        )
+        acl = proxmox.Proxmox(pol, EXP_INFO)
+        output = str(acl)
+        self.assertIn('IN ACCEPT', output)
+        self.assertIn('OUT ACCEPT', output)
         print(output)
 
 
