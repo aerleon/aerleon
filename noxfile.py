023b3c72e1b716905562f9a3e4320a4e6eb96f40
Jason Benterou <jason.benterou@gmail.com>
2025-12-08 12:00:45 -0800

    Remove Python 3.9 as a supported target (EOL)

--- noxfile.py
+++ noxfile.py
@@ -11,7 +11,7 @@ nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ['test']
 
 
-@session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "3.14", "python3.14t"])
+@session(python=["3.10", "3.11", "3.12", "3.13", "3.14", "python3.14t"])
 def test(session):
     """Runs pytest"""
     session.run_always("poetry", "install", external=True)
