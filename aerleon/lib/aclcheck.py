891a154bbf0ed636dfcd7f807b58ca119be559e2
Anders Bruihler <6530276+ABruihler@users.noreply.github.com>
2026-02-06 21:21:24 -0500

    improve nacaddr subnet_of and supernet_of

--- aerleon/lib/aclcheck.py
+++ aerleon/lib/aclcheck.py
@@ -351,7 +351,9 @@ class AclCheck:
         return True
 
     def _AddrMatch(
-        self, addr: nacaddr.IPv4 | nacaddr.IPv6 | Literal["any"], addresses
+        self,
+        addr: nacaddr.IPv4 | nacaddr.IPv6 | Literal["any"],
+        addresses: list[nacaddr.IPv4 | nacaddr.IPv6],
     ) -> Literal["full", "partial", False]:
         """Check if an address matches another address or group of addresses,
         as a full match, partial match, or no match.
